6c6c1e6ea158e67f100cbc842dacf200ce58b569
Muhammad Choeruman Syah <herumansyah28@gmail.com>
2022-07-15 11:31:08 +0700

    commituas

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # UASPemrograman
+
+``` http://choeruuaspemrograman.epizy.com/ ```
+
+[img1](/img/Screenshot%20(401).png)
+
+[img2](/img/Screenshot%20(405).png)
+
+[img3](/img/Screenshot%20(406).png)
+
+[img4](/img/Screenshot%20(407).png)
+
+[img5](/img/Screenshot%20(408).png)
---
+++ ci4/app/Views/template/footer.php
@@ -0,0 +1,21 @@
+</section>
+<aside id="sidebar">
+    <div class="widget-box">
+        <h3 class="title">Widget Header</h3>
+        <ul>
+            <li><a href="<?=base_url('/faqs');?>">FAQ</a></li>
+            <li><a href="<?=base_url('/tos');?>">Terms of Services</a></li>
+        </ul>
+    </div>
+    <div class="widget-box">
+        <h3 class="title">Widget Text</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias et quisquam officia libero provident repellat tempora voluptatum alias sapiente nam fugiat voluptas facilis dolorum, aliquid dolor blanditiis dicta, iste maiores.</p>
+    </div>
+</aside>
+</section>
+<footer>
+    <p>&copy; 2022 - Universitas Pelita Bangsa</p>
+</footer>
+</di>
+</body>
+</html>
